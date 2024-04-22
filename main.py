b4d9ee4b5f051d16cfdce58df077b717ea3e77da
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-04-22 13:57:15 +0800

    Update main.py
    fixed broken assets

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ if __name__ == "__main__":
    import os
    os.system("pip install pysimplegui")
    os.system("pip install pyperclip")
-   os.system("python gui.py")
+   from passwordgenerator import gui
    
