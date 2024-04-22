db211c106e732a96cdae90e20da68a4605c81006
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-04-22 12:00:23 +0800

    Update main.py
    fixed issues

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ if __name__ == "__main__":
    import os
    os.system("pip install pysimplegui")
    os.system("pip install pyperclip")
-   from passwordgenerator import gui
+   os.system("python gui.py")
    
