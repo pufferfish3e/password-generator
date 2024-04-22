ab379979fb4408c5ac29c948126793abfb67cc1e
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-04-22 14:03:56 +0800

    Update installer.py

--- installer.py
+++ installer.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
    import os
    os.system("pip install pysimplegui")
    os.system("pip install pyperclip")
-   print("Install is done! run \`python main.py\` to continue!")
+   print("Install is done! run python main.py to continue!")
